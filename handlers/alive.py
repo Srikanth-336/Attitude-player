177644a7ffa84bf5c60045ca36c656b977fe8c12
Srikanth-336 <97719505+Srikanth-336@users.noreply.github.com>
2022-01-15 17:38:05 +0530

    Update alive.py

--- handlers/alive.py
+++ handlers/alive.py
@@ -1,20 +1,20 @@
-# https://t.me/Alone_shaurya_king
+# https://t.me/Srikanth_36
 
 from modules.helpers.filters import command
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(command(["start", f"start@Attitude_Player_Bot"]) & filters.group & ~filters.edited)
+@Client.on_message(command(["start", f"start@Sriki_Music_Player_Bot"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/be59d5c0d55a63ba62193.jpg",
+        photo=f"https://telegra.ph/file/c1f3bb73d56a201431783.jpg",
         caption=f"""
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🔥🥂ᴊᴏɪɴ ᴏᴜʀ ꜱᴜᴩᴩᴏʀᴛ🥂🔥", url=f"https://t.me/sweetkingdom1")
+                        "🔥🥂ᴊᴏɪɴ ᴏᴜʀ ꜱᴜᴩᴩᴏʀᴛ🥂🔥", url=f"https://t.me/Kings_World_36")
                 ]
             ]
         ),
@@ -24,14 +24,14 @@ async def start(client: Client, message: Message):
 @Client.on_message(command(["help", f"help@Attitude_Player_Bot"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/be59d5c0d55a63ba62193.jpg",
+        photo=f"https://telegra.ph/file/c1f3bb73d56a201431783.jpg",
         caption=f"""
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🔥🥂ᴄʟɪᴄᴋ ʜᴇʀᴇ ꜰᴏʀ ʜᴇʟᴩ🥂🔥", url=f"https://t.me/sweetkingdom1")
+                        "🔥🥂ᴄʟɪᴄᴋ ʜᴇʀᴇ ꜰᴏʀ ʜᴇʟᴩ🥂🔥", url=f"https://t.me/Kings_World_36")
                 ]
             ]
         ),
